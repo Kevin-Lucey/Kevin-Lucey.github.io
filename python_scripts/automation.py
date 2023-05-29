07c001f621213c2f14d5563ee5f2169f2df5dac5
Kevin-Lucey <kevlucey@sbcglobal.net>
2023-05-28 22:19:05 -0400

    automation.py modified

--- python_scripts/automation.py
+++ python_scripts/automation.py
@@ -1,16 +1,23 @@
 import bs4
 import pandas as pd
 
-with open("C:/Users/kevlu/OneDrive/Desktop/Baseball/Kevin-Lucey.github.io/index.html") as f:
-    txt = f.read()
-    soup = bs4.BeautifulSoup(txt, "html.parser")
 
-print(soup.text)
-new_tag = soup.new_tag("div", attrs={"id":"test"})
-new_tag.string = "TEST"
-print(new_tag.text)
+class addHTML:
+    def __init__(self, txt_to_add:str):
+        with open("C:/Users/kevlu/OneDrive/Desktop/Baseball/Kevin-Lucey.github.io/index.html") as f:
+            txt = f.read()
+            soup = bs4.BeautifulSoup(txt, "html.parser")
+        print(soup.text)
+        new_tag = soup.new_tag("div", attrs={"id":"test"})
+        new_tag.string = txt_to_add
+        print(new_tag.text)
 
-soup.html.body.append(new_tag)
+        soup.html.body.append(new_tag)
 
-with open("C:/Users/kevlu/OneDrive/Desktop/Baseball/Kevin-Lucey.github.io/index.html", "w") as outf:
-    outf.write(str(soup))
+        with open("C:/Users/kevlu/OneDrive/Desktop/Baseball/Kevin-Lucey.github.io/index.html", "w") as outf:
+            outf.write(str(soup))
+
+
+
+
+
